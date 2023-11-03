cddf44e6b81f2fb89a2fb1d28da2c1051127203d
H-SM <harmanmalht777@gmail.com>
2023-11-03 22:04:22 +0530

    Added various sectoins for the UI

---
+++ graph_path_guru/src/components/aboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const aboutUs = () => {
+  return (
+    <div>aboutUs</div>
+  )
+}
+
+export default aboutUs
---
+++ graph_path_guru/src/components/docpage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const docpage = () => {
+  return (
+    <div>docpage</div>
+  )
+}
+
+export default docpage
