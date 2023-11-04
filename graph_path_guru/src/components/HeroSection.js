01fe0516a21f8c4736f370ffd664c139c94d31aa
H-SM <harmanmalht777@gmail.com>
2023-11-04 14:59:36 +0530

    Made our Navbar

--- graph_path_guru/src/components/HeroSection.js
+++ graph_path_guru/src/components/HeroSection.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import imager from '../assests/beams-pricing.png'
 const HeroSection = () => {
   return (
     <>
     <div>HeroSection</div>
+    {/* <div class="relative bg-slate-50 pb-16 pt-[5.75rem]">
+      <img src={imager} alt="" class="absolute left-0 top-0 w-[90.4375rem] max-w-none z-[-1]"/>
+      <div class="relative z-20 mx-auto max-w-container px-4 pt-14 sm:px-6 sm:pt-20 lg:px-8"><div class="mx-auto max-w-[45rem] text-center"><h1 class="text-base font-semibold leading-7 text-sky-500">All-access</h1><p class="mt-4 text-5xl font-extrabold leading-[3.5rem] tracking-tight text-slate-900">Get everything, forever.</p><p class="mt-4 text-lg text-slate-700">Get lifetime access to 500+ components and every template available today, plus any new content we add in the future for a simple one-time price.</p></div></div></div> */}
+
     <div style={{ display: "flex", justifyContent: "center", fontWeight: "bold", fontSize: "22px", marginTop: "22px" }}>
         GraphPathGuru
       </div>
