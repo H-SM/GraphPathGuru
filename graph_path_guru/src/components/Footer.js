69520b5ec0d8c1f46125416e6da1897131527e38
H-SM <harmanmalht777@gmail.com>
2023-11-05 14:55:59 +0530

    added our techstack section

--- graph_path_guru/src/components/Footer.js
+++ graph_path_guru/src/components/Footer.js
@@ -3,15 +3,18 @@ import logo from '../assests/logo.png';
 
 const Footer = () => {
   return (
-    <footer class="mx-auto mt-32 w-full max-w-container px-4 sm:px-6 lg:px-8">
-      <div class="border-t border-slate-900/5 py-10">
-        <div className='flex justify-center items-center'>
-      <img src={logo} alt="logo_here" className='w-[30vh]'/>
-      </div>
-    <p class="mt-5 text-center text-sm leading-6 text-slate-500">© 2023 GraphPathGuru. All rights reserved.</p>
-    </div>
-    </footer>
-  )
-}
+    <>
+      <footer className="mx-auto mt-32 w-full max-w-container px-4 sm:px-6 lg:px-8 relative">
+        <div className="border-t border-slate-900/5 py-10">
+          <div className="flex justify-center items-center">
+            <img src={logo} alt="logo_here" className="w-[30vh]" />
+          </div>
+          <p className="mt-5 text-center text-sm leading-6 text-slate-500">© 2023 GraphPathGuru. All rights reserved.</p>
+        </div>
+      </footer>
+      
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- graph_path_guru/src/components/Signup.js
+++ graph_path_guru/src/components/Signup.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Signup = () => {
   return (
     <div>
-      
+      SignUp
     </div>
   )
 }
