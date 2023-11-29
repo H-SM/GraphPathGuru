8b538911086339e52f1c27388a9e4dd75a2ac0bc
your.user.name <harmanmalht777@gmail.com>
2023-11-29 10:21:58 +0530

    removed forget password from server

--- server/models/Forgot.js
+++ server/models/Forgot.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+// const mongoose = require("mongoose");
+// const { Schema } = mongoose;
 
-const ForgotSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },    
-  token: {
-    type: String,
-    required: true,
-  },
-});
+// const ForgotSchema = new Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },    
+//   token: {
+//     type: String,
+//     required: true,
+//   },
+// });
 
-module.exports = mongoose.model("forgot", ForgotSchema);
+// module.exports = mongoose.model("forgot", ForgotSchema);
